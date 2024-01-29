3a0a559d9e10f6ce1b198a54c0494b6746752ff4
Davidgomez183 <davidgomezduque03@gmail.com>
2024-01-29 16:27:24 +0100

    obtener info del login

--- app/src/main/java/com/example/quizmaster/MainActivity.kt
+++ app/src/main/java/com/example/quizmaster/MainActivity.kt
@@ -1,12 +1,37 @@
 package com.example.quizmaster
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var editTextName: EditText
+    private lateinit var editTextNumberOfQuestions: EditText
+    private lateinit var buttonEnter: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Inicializar los elementos de la interfaz
+        editTextName = findViewById(R.id.editTextName)
+        editTextNumberOfQuestions = findViewById(R.id.editTextText)
+        buttonEnter = findViewById(R.id.buttonEnter)
+
+        // Configurar un listener para el botón
+        buttonEnter.setOnClickListener {
+            // Obtener los valores de los EditText
+            val nombre = editTextName.text.toString()
+            val numeroPreguntas = editTextNumberOfQuestions.text.toString()
+
+            // Puedes hacer lo que quieras con los valores, por ejemplo, mostrarlos en un Toast
+            // o pasarlos a otra actividad
+            // Mostrar la información en un Toast
+            val mensaje = "Nombre: $nombre\nNúmero de Preguntas: $numeroPreguntas"
+            Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show()
+        }
     }
-}
+
+    }
