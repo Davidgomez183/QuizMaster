ab77a0837f2db3e958817e19e19ab56390dcdeaf
Davidgomez183 <davidgomezduque03@gmail.com>
2024-03-17 17:46:14 +0100

    Creacion de tabla

--- QuizMasterV2/app/src/main/java/com/example/lamevaprimeraaplicaci/ResultadoActivity.kt
+++ QuizMasterV2/app/src/main/java/com/example/lamevaprimeraaplicaci/ResultadoActivity.kt
@@ -1,6 +1,8 @@
 package com.example.lamevaprimeraaplicaci
 
 import android.os.Bundle
+import android.widget.TableLayout
+import android.widget.TableRow
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -37,40 +39,109 @@ class ResultadoActivity : AppCompatActivity() {
         } catch (e: ClassCastException) {
             e.printStackTrace()
         }
+
+        /*// Obtener una referencia al botón
+        val buttonPlayAgain = findViewById<Button>(R.id.buttonPlayAgain)*/
+
+        // Configurar el OnClickListener
+       /* buttonPlayAgain.setOnClickListener {
+            // Aquí dentro puedes navegar a otro fragmento
+            val nextFragment = FirstFragment() // Reemplaza TuNuevoFragmento con el nombre de tu fragmento
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.FirstFragment, nextFragment)
+                .commit()
+        }*/
+        // Configurar el OnClickListener para el botón de "Salir"
+        /*val buttonExit = findViewById<Button>(R.id.buttonExit)
+        buttonExit.setOnClickListener {
+            // Cerrar la actividad actual y todas las actividades asociadas a ella
+            finishAffinity()
+        }*/
+        // Obtener referencias a los TextView
+        val textViewHeader1 = findViewById<TextView>(R.id.textViewHeader1)
+        val textViewHeader2 = findViewById<TextView>(R.id.textViewHeader2)
+
+// Establecer el texto de los TextView
+        textViewHeader1.text = "Nombre"
+        textViewHeader2.text = "Puntaje"
+
     }
 
     private fun updateView() {
         GlobalScope.launch {
             val allPersons = personDao.getAllPersons() // Obtener todos los registros de la base de datos
-            val allPersonsString = buildString {
-                append("Marcador:\n")
-                allPersons.forEachIndexed { index, person ->
-                    append("${index + 1}. Nombre: ${person.name}, Puntaje: ${person.score}\n")
-                }
-            }
-            // Mostrar todos los registros en el TextView
+            val sortedPersons = allPersons.sortedByDescending { it.score } // Ordenar la lista de personas por puntaje de mayor a menor
+
             runOnUiThread {
-                val textViewResult = findViewById<TextView>(R.id.textViewResult)
-                textViewResult.text = allPersonsString
+                val tableLayout = findViewById<TableLayout>(R.id.tableLayout)
+                tableLayout.removeAllViews() // Limpiar todas las filas existentes antes de agregar nuevas filas
+
+                // Agregar cabecera de la tabla
+                val headerRow = TableRow(this@ResultadoActivity)
+                val headerNames = listOf("Nombre", "Puntaje", "Dificultad")
+                headerNames.forEach { headerName ->
+                    val textView = TextView(this@ResultadoActivity).apply {
+                        text = headerName
+                        layoutParams = TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT).apply {
+                            weight = 10f
+                            marginStart = 16 // Ajusta el margen izquierdo según sea necesario
+                            marginEnd = 16 // Ajusta el margen derecho según sea necesario
+                            width = 0 // Establece el ancho en 0 para que el weight funcione correctamente
+                        }
+                    }
+                    headerRow.addView(textView)
+                }
+                tableLayout.addView(headerRow)
+
+                // Agregar datos de los jugadores
+                sortedPersons.forEach { person ->
+                    val tableRow = TableRow(this@ResultadoActivity)
+
+                    val nameTextView = TextView(this@ResultadoActivity).apply {
+                        text = person.name
+                        layoutParams = TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT).apply {
+                            weight = 10f
+                            marginStart = 50 // Ajusta el margen izquierdo según sea necesario
+                            marginEnd = 16 // Ajusta el margen derecho según sea necesario
+                            width = 50 // Establece el ancho en 0 para que el weight funcione correctamente
+                        }
+                    }
+                    val scoreTextView = TextView(this@ResultadoActivity).apply {
+                        text = person.score.toString()
+                        layoutParams = TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT).apply {
+                            weight = 1f
+                        }
+                    }
+                    val difficultyTextView = TextView(this@ResultadoActivity).apply {
+                        text = person.dificil
+                        layoutParams = TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT).apply {
+                            weight = 1f
+                        }
+                    }
+
+                    tableRow.addView(nameTextView)
+                    tableRow.addView(scoreTextView)
+                    tableRow.addView(difficultyTextView)
+
+                    tableLayout.addView(tableRow)
+                }
             }
         }
     }
+
+
     private fun insertPerson(person: Person) {
         GlobalScope.launch {
             // Insertar los datos en la base de datos
             personDao.insert(listOf(person))
 
             runOnUiThread {
-                // Obtener el TextView del layout
-                val textViewResult = findViewById<TextView>(R.id.textViewResult)
-                // Actualizar el TextView con los valores de person.name y person.score
-                textViewResult.text = "Nombre: ${person.name}, Puntaje: ${person.score}"
-
                 // Llamar a updateView para mostrar todos los jugadores, incluido el nuevo jugador insertado
                 updateView()
             }
         }
     }
+
 
 
     private fun showToast(message: String) {
